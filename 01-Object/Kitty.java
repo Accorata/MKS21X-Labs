544dc89b6ea54770c684aebf2bc4be8afa573b41
Henry Bach <henrypbach@gmail.com>
2021-10-21 10:01:06 -0400

    Kitty Working

---
+++ 01-Object/Kitty.java
@@ -0,0 +1,24 @@
+public class Kitty {
+  private int age;
+  private String name;
+  public Kitty (){
+    age = 2;
+    name = "Mittens";
+  }
+  public Kitty (String startName, int startAge){
+    age = startAge;
+    name = startName;
+  }
+  public int getAge(){
+    return age;
+  }
+  public String getName(){
+    return name;
+  }
+  public void makeOlder(){
+    age++;
+  }
+  public void changeName(String newName){
+    name = newName;
+  }
+}

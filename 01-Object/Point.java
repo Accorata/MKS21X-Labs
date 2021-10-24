b5ad496042daf920a3d4595580dc901fe6d07a71
Henry Bach <henrypbach@gmail.com>
2021-10-24 10:46:57 -0400

    point complete

--- 01-Object/Point.java
+++ 01-Object/Point.java
@@ -14,4 +14,12 @@ public class Point{
   public double getY(){
     return y;
   }
+
+  public static double distance(Point p1, Point p2){
+    return Math.sqrt(Math.pow(p1.getX()-p2.getX(), 2)+Math.pow(p1.getY()-p2.getY(), 2));
+  }
+
+  public double distanceTo(Point p2){
+    return Math.sqrt(Math.pow(x-p2.getX(), 2)+Math.pow(y-p2.getY(), 2));
+  }
 }

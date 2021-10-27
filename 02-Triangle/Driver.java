d1c4ffcae24544d888579bb76fb772111f543edb
Henry Bach <henrypbach@gmail.com>
2021-10-26 21:40:13 -0400

    Driver Complete + Tests

--- 02-Triangle/Driver.java
+++ 02-Triangle/Driver.java
@@ -8,16 +8,25 @@ public class Driver{
 
     Triangle T1 = new Triangle (P1, P3, P4);
     Triangle T2 = new Triangle (0, 0, 3, 4, 0, 4);
-    Triangle T3 = new Triangle (P1, P3, P5);
+    Triangle T4 = new Triangle (0.000000001, 0, 2.9999999, 4, 0, 3.999999);
+    Triangle T3 = new Triangle (P1, P3, P4);
+    Triangle T6 = new Triangle (1,1, 1,2, 2,2);
+    Triangle T5 = new Triangle (0,0,1,0,0.5,0.86602540378);
 
     System.out.println(T1);
     System.out.println(T2);
     System.out.println(T1.getVertex(2));
     System.out.println(T3.getVertex(3));
     System.out.println(T3);
-    T3.setVertex(2, P4);
-    System.out.println(T3);
-    System.out.println(T1.getPerimeter());
+    System.out.println(T3.classify());
+    System.out.println(T6.classify());
+    System.out.println(T3.area());
     System.out.println(T2.getPerimeter());
+    System.out.println(T2.area());
+    System.out.println(T3.equals(T4));
+    System.out.println(T3.equals(T1));
+    System.out.println(T3.classify());
+    System.out.println(T5.classify());
+
   }
  }

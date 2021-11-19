eb9bafa2a9eb4c772f44f3fb6f911f925e6ebd43
Henry Bach <henrypbach@gmail.com>
2021-11-19 10:15:54 -0500

    Hw:

---
+++ 05-ArrayList/UserInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class UserInput {
+  public static void main(String[] args){
+    String final = "";
+    int min, max = 0;
+    Scanner in = new Scanner (System.in);
+    System.out.println("Enter a non negative number. A negative number will end the program:");
+
+  }
+}

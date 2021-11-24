7c2689fb978ed90c879f04ba6460fc0546e92319
Henry Bach <henrypbach@gmail.com>
2021-11-24 09:44:04 -0500

    CW 11/24

--- 06-Advent/Wrapping.java
+++ 06-Advent/Wrapping.java
@@ -5,7 +5,7 @@ public class Wrapping {
     File data = new File (args[0]);
     int mode = Integer.parseInt(args[1]);
     if (mode == 1) System.out.println(problemOne(data));
-    //if (mode == 2) System.out.println(problemTwo(data));
+    if (mode == 2) System.out.println(problemTwo(data));
   }
   public static int problemOne(File data){
     try {
@@ -24,6 +24,31 @@ public class Wrapping {
         if (points[1] * points[2] < min) min = points[1] * points[2];
         if (points[0] * points[2] < min) min = points[0] * points[2];
         total += 2 * points[0] * points[1] + 2 * points[1] * points[2] + 2 * points[0] * points[2] + min;
+      }
+      input.close();
+      return total;
+    } catch (FileNotFoundException e){
+      e.printStackTrace();
+    }
+    return 2000;
+  }
+  public static int problemTwo(File data){
+    try {
+      //W.txt
+      Scanner input = new Scanner(data);
+      String[] newLine = new String[3];
+      int[] points = new int[3];
+      int total = 0;
+      int min = 0;
+      while (input.hasNext()){
+        newLine = input.nextLine().split("x");
+        for (int i = 0; i<3; i++){
+          points[i] = Integer.parseInt(newLine[i]);
+        }
+        min = points[0] + points[1];
+        if (points[1] + points[2] < min) min = points[1] + points[2];
+        if (points[0] + points[2] < min) min = points[0] + points[2];
+        total += points[0] * points[1] * points[2] + 2 * min;
       }
       input.close();
       return total;

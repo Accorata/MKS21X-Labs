f8d81b984d112f9fde8eb536675af411456cd4a6
Henry Bach <henrypbach@gmail.com>
2021-12-16 18:57:45 -0500

    HW 12/16: Completed + Tested

--- 11-ExtendArrayList/NoNullArrayList.java
+++ 11-ExtendArrayList/NoNullArrayList.java
@@ -1,15 +1,30 @@
 import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T> {
-  public boolean set(int index, T t){
-    if (t == null) return false;
-    else {
-      return this.set(index, t);
+  public NoNullArrayList() {
+    super();
+  }
+  public NoNullArrayList(int startingCapacity) {
+    super(startingCapacity);
+  }
+  public T set(int index, T t){
+    if (t == null) {
+      throw new IllegalArgumentException ("NoNullArrayList does not accept null values");
+    } else {
+      return super.set(index, t);
     }
   }
   public boolean add(T t){
-    if (t == null) return false;
-    else {
-      return this.add(t);
+    if (t == null) {
+      throw new IllegalArgumentException ("NoNullArrayList does not accept null values");
+    } else {
+      return super.add(t);
+    }
+  }
+  public void add(int index, T t){
+    if (t == null) {
+      throw new IllegalArgumentException ("NoNullArrayList does not accept null values");
+    } else {
+      super.add(index, t);
     }
   }
 }
--- 11-ExtendArrayList/Test.java
+++ 11-ExtendArrayList/Test.java
@@ -1,5 +1,15 @@
 public class Test{
   public static void main(String[] args){
-
+    NoNullArrayList<String> a = new NoNullArrayList<String>();
+    NoNullArrayList<Integer> b = new NoNullArrayList<Integer>(5);
+    System.out.println(a.add("test"));
+    //System.out.println(a.add(null));
+    System.out.println(b.add(1));
+    b.add(0,4);
+    //b.add(1,null);
+    System.out.println(a.set(0, "testing"));
+    //System.out.println(b.set(0, null));
+    System.out.println(a);
+    System.out.println(b);
   }
 }

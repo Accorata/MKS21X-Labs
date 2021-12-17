5eb6be80537a32561770a47600800f9ea2b9c61f
Henry Bach <henrypbach@gmail.com>
2021-12-17 10:11:00 -0500

    CW 12/17: Int testing

--- 11-ExtendArrayList/Test.java
+++ 11-ExtendArrayList/Test.java
@@ -2,7 +2,7 @@ public class Test{
   public static void main(String[] args){
     OrderedArrayList<String> a = new OrderedArrayList<String>();
     NoNullArrayList<Integer> b = new NoNullArrayList<Integer>(5);
-    OrderedArrayList<String> c = new OrderedArrayList<String>(3);
+    OrderedArrayList<Integer> c = new OrderedArrayList<Integer>(3);
     System.out.println(a.add("test"));
     System.out.println(a.add("abc"));
     System.out.println(a.add("abc"));
@@ -19,6 +19,14 @@ public class Test{
     //System.out.println(b.set(0, null));
     System.out.println(a);
     //System.out.println(b);
-    a.add(null);
+    //a.add(null);
+    c.add(56);
+    c.add(456);
+    c.add(-45678);
+    c.add(-1);
+    c.add(1);
+    System.out.println(c);
+    System.out.println(c.set(0, 2000000));
+    System.out.println(c);
   }
 }

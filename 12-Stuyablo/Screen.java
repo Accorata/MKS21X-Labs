37cf62cde6af128f1d8de822faa910b70c861045
Henry Bach <henrypbach@gmail.com>
2022-01-11 09:59:58 -0500

    2D border

--- 12-Stuyablo/Screen.java
+++ 12-Stuyablo/Screen.java
@@ -5,6 +5,14 @@ public class Screen {
     for(int i = 0; i<30; i++){
       Text.go(i+1,1);
       System.out.println(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
+      //Text.go(i+1,80);
+      //System.out.println(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
     }
+    for(int i = 0; i<80; i++){
+      Text.go(1,i+1);
+      System.out.println(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
+    }
+    Text.go(31,1);
+    Text.showCursor();
   }
 }

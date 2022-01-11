3cfd7a69ccd9bf4a946d73333eb80d5e509d71e1
Henry Bach <henrypbach@gmail.com>
2022-01-11 10:15:47 -0500

    With some numbers

--- 12-Stuyablo/Screen.java
+++ 12-Stuyablo/Screen.java
@@ -5,18 +5,21 @@ public class Screen {
     for(int i = 0; i<30; i++){
       Text.go(i,0);
       System.out.println(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
-    // }
-    // for(int i = 0; i<30; i++){
       Text.go(i,79);
       System.out.println(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
     }
     for(int i = 0; i<80; i++){
       Text.go(0,i);
       System.out.println(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
-    // }
-    // for(int i = 0; i<80; i++){
       Text.go(29,i);
       System.out.println(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
+    }
+    for(int i = 0; i<4; i++){
+      int num = (int)(Math.random()*100);
+      Text.go(3,16+i*16);
+      int color = Text.BLACK;
+      if (num < 25) color = Text.RED;
+      System.out.println(Text.colorize("20",color));
     }
     Text.go(30,1);
     Text.showCursor();

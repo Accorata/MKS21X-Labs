32ebec3ca957df13443499ffc301083669fccdfc
Henry Bach <henrypbach@gmail.com>
2022-01-11 10:10:38 -0500

    4D screen

--- 12-Stuyablo/Screen.java
+++ 12-Stuyablo/Screen.java
@@ -3,16 +3,22 @@ public class Screen {
     Text.hideCursor();
     Text.clear();
     for(int i = 0; i<30; i++){
-      Text.go(i+1,1);
+      Text.go(i,0);
       System.out.println(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
-      //Text.go(i+1,80);
-      //System.out.println(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
+    // }
+    // for(int i = 0; i<30; i++){
+      Text.go(i,79);
+      System.out.println(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
     }
     for(int i = 0; i<80; i++){
-      Text.go(1,i+1);
+      Text.go(0,i);
+      System.out.println(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
+    // }
+    // for(int i = 0; i<80; i++){
+      Text.go(29,i);
       System.out.println(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
     }
-    Text.go(31,1);
+    Text.go(30,1);
     Text.showCursor();
   }
 }

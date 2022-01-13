bab1ceec798dcf20698bb4dc2d2292c3beb3c60c
Henry Bach <henrypbach@gmail.com>
2022-01-13 06:51:53 -0500

    New game change

--- 12-Stuyablo/Game.java
+++ 12-Stuyablo/Game.java
@@ -1,22 +1,49 @@
-public class Game{
-  public static void main(String[] args) {
-    //Adventurer a = new Warrior("Conan","Aaaaaaaar!",20);
-    //Adventurer b = new Warrior("Homer","Donuts?",0);
-    Adventurer a = new Warrior();
-    Adventurer b = new Wizard("Eldor",3,"Cryomancy");
-    System.out.println(a+" ("+a.getHP()+"HP)");
-    System.out.println(b+" ("+b.getHP()+"HP)");
-    a.attack(b);
-    b.attack(a);
-    System.out.println(a+" ("+a.getHP()+"HP)");
-    System.out.println(b+" ("+b.getHP()+"HP)");
-    a.specialAttack(b);
-    b.specialAttack(a);
-    System.out.println(a+" ("+a.getHP()+"HP)");
-    System.out.println(b+" ("+b.getHP()+"HP)");
-    a.attack(b);
-    b.attack(a);
-    System.out.println(a+" ("+a.getHP()+"HP)");
-    System.out.println(b+" ("+b.getHP()+"HP)");
+import java.util.Scanner;
+public class Game {
+  public static void main(String[] args){
+    int[] num = randFour();
+    display(num);
+    Scanner input = new Scanner (System.in);
+    String last = "";
+    while(last != "q" && last != "quit"){
+      if (last != "") num = randFour();
+      display(num);
+      last = input.nextLine().toLowerCase();
+      //last = "q";
+    }
+    input.close();
+  }
+  private static void display(int[] num){
+    Text.hideCursor();
+    Text.clear();
+    for(int i = 0; i<30; i++){
+      Text.go(i,0);
+      System.out.println(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
+      Text.go(i,79);
+      System.out.println(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
+    }
+    for(int i = 0; i<80; i++){
+      Text.go(0,i);
+      System.out.print(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
+      Text.go(29,i);
+      System.out.print(Text.colorize(" ",Text.GREEN+Text.BACKGROUND));
+    }
+    for(int i = 0; i<4; i++){
+      Text.go(3,16+i*16);
+      int color = Text.WHITE;
+      if (num[i] < 25) color = Text.RED;
+      if (num[i] > 75) color = Text.GREEN;
+      System.out.println(Text.colorize(""+num[i],color));
+    }
+    Text.go(30,1);
+    Text.showCursor();
+    System.out.print(Text.colorize(">",Text.GREEN));
+  }
+  private static int[] randFour(){
+    int[] num = new int[4];
+    for(int i = 0; i<4; i++){
+      num[i] = (int)(Math.random()*100);
+    }
+    return num;
   }
 }
---
+++ 12-Stuyablo/Game_old.java
@@ -0,0 +1,22 @@
+public class Game_old{
+  public static void main(String[] args) {
+    //Adventurer a = new Warrior("Conan","Aaaaaaaar!",20);
+    //Adventurer b = new Warrior("Homer","Donuts?",0);
+    Adventurer a = new Warrior();
+    Adventurer b = new Wizard("Eldor",3,"Cryomancy");
+    System.out.println(a+" ("+a.getHP()+"HP)");
+    System.out.println(b+" ("+b.getHP()+"HP)");
+    a.attack(b);
+    b.attack(a);
+    System.out.println(a+" ("+a.getHP()+"HP)");
+    System.out.println(b+" ("+b.getHP()+"HP)");
+    a.specialAttack(b);
+    b.specialAttack(a);
+    System.out.println(a+" ("+a.getHP()+"HP)");
+    System.out.println(b+" ("+b.getHP()+"HP)");
+    a.attack(b);
+    b.attack(a);
+    System.out.println(a+" ("+a.getHP()+"HP)");
+    System.out.println(b+" ("+b.getHP()+"HP)");
+  }
+}

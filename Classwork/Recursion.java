43dcec069fe03878e52ed3d62b6195fff327c5b8
Henry Bach <henrypbach@gmail.com>
2022-01-27 09:58:59 -0500

    fibonacci

--- Classwork/Recursion.java
+++ Classwork/Recursion.java
@@ -1,7 +1,7 @@
 public class Recursion {
   public static void main(String[] args){
     int n = Integer.parseInt(args[0]);
-    System.out.println(isEven(n));
+    System.out.println(fibonacci(n));
   }
   public static int fact(int n){
     if (n == 0) return 1;
@@ -12,5 +12,9 @@ public class Recursion {
     if (x == 1) return false;
     if (x < 0) return isEven(-x);
     return isEven(x-2);
+  }
+  public static int fibonacci(int n) {
+    if (n <= 1) return n;
+    return fibonacci(n-1)+fibonacci(n-2);
   }
 }
